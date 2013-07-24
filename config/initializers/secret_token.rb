9248d1517e4835d7115a1ca0125ac8147da8cfd9
calin <calinoiu.alexandru@agilefreaks.com>
2013-07-24 21:34:47 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apibugfreak::Application.config.secret_key_base = 'c5c1aff06084ea0bb85a05b23acd11acadcc3a3aafb991ef24aa2fb941e2b128cf789550a403c935dc4ac261fcbedda0de6df837360dd03593a13105c3ae1580'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Apibugfreak::Application.config.session_store :cookie_store, key: '_apibugfreak_session'
