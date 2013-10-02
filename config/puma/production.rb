db40757d9b77b5b591beb7843e374571bb09c6f1
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-02 15:06:47 +0300

    adds production config for puma

---
+++ config/puma/production.rb
@@ -0,0 +1,7 @@
+threads 2,16
+
+bind 'unix:///var/www/bugfreak/shared/sockets/puma.sock'
+pidfile '/var/www/bugfreak/shared/sockets/puma.pid'
+state_path '/var/www/bugfreak/shared/sockets/puma.state'
+
+activate_control_app
