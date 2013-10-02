9c51d80af8d845bd0b874ea49207649697495b00
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-02 17:30:39 +0300

    update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,5 @@ bind 'unix:///var/www/bugfreak/shared/sockets/puma.sock'
 pidfile '/var/www/bugfreak/shared/sockets/puma.pid'
 state_path '/var/www/bugfreak/shared/sockets/puma.state'
 
-stdout_redirect '/var/www/bugfreak/shared/log/pumaout.log', 'var/www/bugfreak/shared/log/pumaerr.log', true
-
 activate_control_app 'unix:///var/www/bugfreak/shared/sockets/pumactl.sock'
 activate_control_app
