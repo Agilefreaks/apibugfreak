db5c844071b37c5f519beb14118f141789783463
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-02 16:54:58 +0300

    updates puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -1,9 +1,14 @@
 rails_env = ENV['RAILS_ENV'] || 'development'
 
+environment rails_env
+
+daemonize true
+
 threads 2,16
 
 bind 'unix:///var/www/bugfreak/shared/sockets/puma.sock'
 pidfile '/var/www/bugfreak/shared/sockets/puma.pid'
 state_path '/var/www/bugfreak/shared/sockets/puma.state'
 
+activate_control_app 'unix:///var/www/bugfreak/shared/sockets/pumactl.sock'
 activate_control_app
