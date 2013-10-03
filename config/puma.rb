b41f04b77049956f43a8fe477fc5f496df793bf9
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-03 21:31:10 +0300

    puma is enviroment aware

--- config/puma.rb
+++ config/puma.rb
@@ -6,9 +6,9 @@ daemonize true
 
 threads 2,16
 
-bind 'unix:///var/www/bugfreak/shared/sockets/puma.sock'
-pidfile '/var/www/bugfreak/shared/sockets/puma.pid'
-state_path '/var/www/bugfreak/shared/sockets/puma.state'
+bind "unix:///var/www/bugfreak_#{rails_env}/shared/sockets/puma.sock"
+pidfile "/var/www/bugfreak_#{rails_env}/shared/sockets/puma.pid"
+state_path "/var/www/bugfreak_#{rails_env}/shared/sockets/puma.state"
 
-activate_control_app 'unix:///var/www/bugfreak/shared/sockets/pumactl.sock'
+activate_control_app "unix:///var/www/bugfreak_#{rails_env}/shared/sockets/pumactl.sock"
 activate_control_app
