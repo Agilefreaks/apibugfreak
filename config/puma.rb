2f5d07c7512c9ff4c310ef79d2e5117d6827e7dc
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-14 22:39:59 +0300

    updates puma config

--- config/puma.rb
+++ config/puma.rb
@@ -10,7 +10,7 @@ bind "unix:///var/www/bugfreak_#{rails_env}/shared/sockets/puma.sock"
 pidfile "/var/www/bugfreak_#{rails_env}/shared/sockets/puma.pid"
 state_path "/var/www/bugfreak_#{rails_env}/shared/sockets/puma.state"
 
-stdout_redirect "/var/www/bugfreak_#{rails_env}/shared/logs/puma_stdout.log", "/var/www/bugfreak_#{rails_env}/shared/logs/puma_stderr.log", true
+stdout_redirect "/var/www/bugfreak_#{rails_env}/shared/log/puma_stdout.log", "/var/www/bugfreak_#{rails_env}/shared/log/puma_stderr.log", true
 
 activate_control_app "unix:///var/www/bugfreak_#{rails_env}/shared/sockets/pumactl.sock"
 activate_control_app
