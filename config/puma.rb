981e50e95c13048f6fd9532e86aa5b6afea47a0b
Calin <calinoiu.alexandru@agilefreaks.com>
2013-10-15 14:43:43 +0300

    removes useless logs

--- config/puma.rb
+++ config/puma.rb
@@ -1,13 +1,11 @@
 rails_env = ENV['RAILS_ENV'] || 'development'
 
-environment 'development'
+environment rails_env
 
-daemonize false
+daemonize true
 
-threads 2,16
+threads 0,16
 
 bind "unix:///var/www/bugfreak_#{rails_env}/shared/sockets/puma.sock"
 pidfile "/var/www/bugfreak_#{rails_env}/shared/sockets/puma.pid"
 state_path "/var/www/bugfreak_#{rails_env}/shared/sockets/puma.state"
-
-stdout_redirect "/var/www/bugfreak_#{rails_env}/shared/log/puma_stdout.log", "/var/www/bugfreak_#{rails_env}/shared/log/puma_stderr.log", true
