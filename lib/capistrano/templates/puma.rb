9ceb25a3caba3df7643bd7662e6960bb926cc836
calin <calinoiu.alexandru@agilefreaks.com>
2014-10-08 20:10:43 +0300

    updates puma config

--- lib/capistrano/templates/puma.rb
+++ lib/capistrano/templates/puma.rb
@@ -1,21 +1,21 @@
 #!/usr/bin/env puma
 
-directory '/var/www/omniapi/current'
-rackup '/var/www/omniapi/current/config.ru'
+directory '/var/www/apibugfreak/current'
+rackup '/var/www/apibugfreak/current/config.ru'
 environment ENV['RACK_ENV']
 
-pidfile '/var/www/omniapi/shared/tmp/pids/puma.pid'
-state_path '/var/www/omniapi/shared/tmp/pids/puma.state'
-stdout_redirect '/var/www/omniapi/shared/log/puma_access.log', '/var/www/omniapi/shared/log/puma_error.log', true
+pidfile '/var/www/apibugfreak/shared/tmp/pids/puma.pid'
+state_path '/var/www/apibugfreak/shared/tmp/pids/puma.state'
+stdout_redirect '/var/www/apibugfreak/shared/log/puma_access.log', '/var/www/apibugfreak/shared/log/puma_error.log', true
 
 threads 0, 1
 
-bind 'unix:/var/www/omniapi/shared/tmp/sockets/puma.sock'
+bind 'unix:/var/www/apibugfreak/shared/tmp/sockets/puma.sock'
 workers 2
 
 preload_app!
 
 on_restart do
   puts 'Refreshing Gemfile'
-  ENV['BUNDLE_GEMFILE'] = '/var/www/omniapi/current/Gemfile'
+  ENV['BUNDLE_GEMFILE'] = '/var/www/apibugfreak/current/Gemfile'
 end
